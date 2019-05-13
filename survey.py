7c7d3b179e2e038eb8575c7a0dc3a626fcf433b4
Carlos Manzanedo Rueda (ruecarlo@) <ruecarlo@amazon.com>
2019-05-13 11:30:55 +0100

    Support of CheckBox questions and simplifying the html nested structure

--- survey.py
+++ survey.py
@@ -26,25 +26,26 @@ def lambda_handler(event, context):
             doc.stag('br')
             
             with tag('div', align='center'):
-                doc.stag('font', size="6", style="font-weight: bold; font-family: verdana; color:#" + str(theme) + ";")  
-                text(title)
-                doc.stag('br')
-                doc.stag('font', size="2", style="font-weight: bold; font-family: verdana")  
-                text("by " + author)
-                doc.stag('br')
-                doc.stag('img', src=image, width="500")
-                doc.stag('br')
-                doc.stag('br')
+                with doc.tag('div', style="font-size: medium;font-weight: bold; font-family: verdana; color:#" + str(theme) + ";"): 
+                    text(title)
+                    doc.stag('br')
+                with doc.tag('div', style="font-size: small; font-weight: bold; font-family: verdana;"):
+                    text("by " + author)
+                    doc.stag('br')
+                    doc.stag('img', src=image, width="500")
+                    doc.stag('br')
+                    doc.stag('br')
             
-            with tag('form', action = "submitsurvey"):
-                with tag('div', style="margin-left: auto; margin-right: auto; width: 40%;"):
-                    for questionName in questionsNames:
+            with tag('form', action = "submitsurvey", style="margin-left: auto; margin-right: auto; width: 70%;"):
+                for questionName in questionsNames:
+                    with tag('div'):
                         questionLabel = questions[questionName]['Label']
                         questionType = questions[questionName]['Type']
-            
-                        doc.stag('font', size="4", style="font-weight: bold; font-family: verdana; color:#" + str(theme) + ";")    
-                        text(questionLabel)
-                        doc.stag('br')
+
+                        #doc.stag('font', size="4", style="font-weight: bold; font-family: verdana; color:#" + str(theme) + ";")    
+                        with doc.tag('div',style="font-size: medium;font-weight: bold; font-family: verdana; color:#" + str(theme) + ";"): 
+                            doc.asis(questionLabel)
+                            doc.stag('br')
                         
                         if (questionType == "Text"):
                             with doc.textarea(name = questionName, style="width: 100%; border-color: #" + str(theme) + "; " , rows="5"):
@@ -58,15 +59,26 @@ def lambda_handler(event, context):
                             values = questions[questionName]['Values']
                             for valueIterator in values:
                                 value = questions[questionName]['Values'][valueIterator]
-                                doc.input(name = questionName, type = 'radio', value = value, style="border-color: #" + str(theme) + "; ")
-                                doc.stag('font', size="2", style="font-weight: normal; font-family: verdana; color:black")
-                                text(str(value))
-                                doc.stag('br')
+                                with doc.tag('div', style="font-size: small; font-weight: normal; font-family: verdana; color:black;"):
+                                    doc.input(name = questionName, type = 'radio', value = value, style="border-color: #" + str(theme) + "; ")
+                                    text(" "+str(value))
+                                    doc.stag('br')
+                                
+                        if (questionType == "CheckBox"):
+                            with tag('fieldset',style="border: 0px; padding: 0px; font-size: small; font-weight: normal; font-family: verdana; color:black;"):
+                                values = list(questions[questionName]['Values'])
+                                for valueIterator in values:
+                                    value = questions[questionName]['Values'][valueIterator]
+                                    field_name = questionName + "_" + "".join([ c if c.isalnum() else "_" for c in value.lower() ])
+                                    doc.input(name = field_name, type = 'hidden', value = "0",style="border-color: #" + str(theme) + "; ")
+                                    doc.input(name = field_name, id = value ,  type = 'checkbox', value = "1", style="border-color: #" + str(theme) + "; ")
+                                    text(" "+str(value))
+                                    doc.stag('br')
                             
                         doc.stag('br')
                         doc.stag('br')
     
-                    doc.stag('input', type = "submit", value = "Send!", style="background-color: #" + str(theme) + "; border: none; color: white; float: right; padding: 15px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer;")
+                doc.stag('input', type = "submit", value = "Send!", style="background-color: #" + str(theme) + "; border: none; color: white; float: right; padding: 15px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer;")
 
 
 
